eeb94715475a4c63f19d3d6e9119706d91081f03
flatironschool <t.benson@live.co.uk>
2020-01-22 00:17:57 +0000

    added seed to make sure every restaurant has tags, completed find lunch form, started method for filtering results

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    @@user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
-    if !(user && user.authenticate(params[:password]))
+    if !(@@user && @@user.authenticate(params[:password]))
       session[:user_id] = nil
       flash[:notice] = "Sorry we can't find those details..."
       redirect_to '/login'
     else
       flash[:notice] = "You have logged in successfully!"
-      session[:user_id] = user.id
+      session[:user_id] = @@user.id
       redirect_to '/'
     end
   end
@@ -21,23 +21,22 @@ class SessionsController < ApplicationController
     redirect_to '/login'
   end
 
-
   def find_lunch_form
-      # set_user
     @areas = Area.all
     @tags = Tag.all
     @price_ranges = Restaurant.price_ranges
   end
 
   def get_lunch
-    puts session_params
-    byebug
+    Session.find_restaurants(session_params)
     redirect_to restaurants_path
   end
 
   private
 
-  def session_params
-    params.require(:session).permit(:user_id, :area_id, :tag_ids)
+  def session_params(*args)
+    params.require(:session).permit(:user_id, :area_id, :price_range, tag_ids: [])
   end
+
+  
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,21 @@
 class Session < ApplicationRecord
-    accepts_nested_attributes_for :tags
+    # helper_method :find_restaurants
+
+
+    # first match by location
+    # then match by tags
+    # then match by price range
+    # custom errors at each step if nothing meeting that description here is something close
+    def self.find_restaurants(params)
+        first_filter = Restaurant.all.select { |r| 
+            r.area_id == params[:area_id].to_i
+            }
+        second_filter = find_restaurants.select { |r| 
+        # finish this method iterating through restaurant tags
+            r.restaurant_tags.include?(restaurant_tags.each ) 
+            }
+            
+        byebug
+    end
 end
+  
